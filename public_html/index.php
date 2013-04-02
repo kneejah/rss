c4e030dfdda0e0827ed84faaa7e3a31f501fea36
Nick Moore <kneejah@gmail.com>
2013-04-01 22:38:31 -0700

    adding files

---
+++ public_html/index.php
@@ -0,0 +1,26 @@
+<?php
+	require '../vendor/autoload.php';
+
+	// The standard PHP autoloader
+	function rss_autoload($name)
+	{
+    	$split = str_replace('_', '/', $name);
+    	$path = "../" . $split . ".php";
+    	$result = require_once($path);
+    	return ($result !== false);
+	}
+	spl_autoload_register('rss_autoload');
+
+	// Autoloader for the Redis ORM stuff
+	function redis_autoload($class) {
+    	if (substr_compare($class, 'redis\\orm\\', 0, 10, false) === 0)
+    	{
+        	$filename = __DIR__ . '/lib/' . strtr(substr($class, 10), '\\', '/') . '.php';
+        	$result = require_once($filename);
+        	return ($result !== false);
+    	}
+    	return false;
+	}
+	spl_autoload_register('redis_autoload');
+
+	Routes_Route::init();
